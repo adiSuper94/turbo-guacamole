418c4832a8f801a18ee3092788e6d436ad8405f1
Aditya Subramanian <aditarun2008@gmail.com>
2023-11-16 21:08:15 -0500

    basic chat server

--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -15,41 +17,92 @@ type MessageType int
 
 const (
 	Text MessageType = iota
-	CloseConnection
+	Login
+	Logout
+	SingleTick
 )
 
 type Message struct {
 	Type MessageType
-	Data []byte
+	Data string
+	To   string
+	From string
 }
 
 type Server struct {
+	clients map[string]*websocket.Conn
+}
+
+func (s Server) HandleMessage(msg Message, ctx context.Context, clientAddr string, clientConn *websocket.Conn) error {
+	switch msg.Type {
+	case Text:
+		clientConn, ok := s.clients[msg.To]
+		if !ok {
+			fmt.Fprintln(os.Stderr, "Error4: `to` is offline")
+			break
+		}
+		msg.From, msg.To = msg.To, msg.From
+		bytes, err := json.Marshal(msg)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error5: ", err)
+			return errors.New("Could not marshal message")
+		}
+		clientConn.Write(ctx, websocket.MessageText, bytes)
+	case Logout:
+		delete(s.clients, clientAddr)
+	case Login:
+		s.clients[clientAddr] = clientConn
+	}
+	return nil
 }
 
 func main() {
-	clients := make(map[string]*websocket.Conn)
+	server := Server{
+		clients: make(map[string]*websocket.Conn),
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		c, err := websocket.Accept(w, r, &websocket.AcceptOptions{InsecureSkipVerify: true})
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error1: ", err)
+			return
 		}
 		defer c.CloseNow()
 		addr := r.RemoteAddr
 		log.Printf("Client connected: %s", addr)
-		ctx, cancel := context.WithTimeout(r.Context(), 30*time.Second)
+		ctx, cancel := context.WithTimeout(r.Context(), 360*time.Second)
 		defer cancel()
-		clients[addr] = c
+		server.clients[addr] = c
+		defer delete(server.clients, addr)
 		for {
-			_, v, err := c.Read(ctx)
+			_, rawMessageBytes, err := c.Read(ctx)
 			if err != nil {
-				delete(clients, addr)
 				fmt.Fprintln(os.Stderr, "Error2: ", err)
 				break
 			}
-			log.Printf("Received: %s", v)
+			var msg Message
+			err = json.Unmarshal(rawMessageBytes, &msg)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error3: ", err)
+				break
+			}
+			msg.From = addr
+			log.Printf("Received: %s", msg.Data)
+			err = server.HandleMessage(msg, ctx, addr, c)
+			if err != nil {
+				break
+			}
 		}
 
 		c.Close(websocket.StatusNormalClosure, "")
+	})
+
+	http.HandleFunc("/activeUsers", func(w http.ResponseWriter, r *http.Request) {
+		var activeUsers []string
+		for k := range server.clients {
+			activeUsers = append(activeUsers, k)
+		}
+		json.NewEncoder(w).Encode(activeUsers)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
