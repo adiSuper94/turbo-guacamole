844bb9e60e4c0026472c5fa6ef664d7a34341208
Aditya Subramanian <aditarun2008@gmail.com>
2024-04-12 22:11:08 -0400

    add ws message spec as a diff module

---
+++ wsmessagespec/go.mod
@@ -0,0 +1,5 @@
+module adisuper94/turboguac/wsmessagespec
+
+go 1.21.3
+
+require github.com/google/uuid v1.6.0
---
+++ wsmessagespec/message.go
@@ -0,0 +1,21 @@
+package wsmessagespec
+
+import "github.com/google/uuid"
+
+type WSMessageType int
+
+const (
+	Text WSMessageType = iota
+	Login
+	LoginAck
+	Logout
+	SingleTick
+)
+
+type WSMessage struct {
+	Id   uuid.UUID // message id
+	Type WSMessageType
+	Data string
+	To   uuid.UUID //chatroom id
+	From string    // self username
+}
