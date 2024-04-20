9a65b0c65dcea5a3771e6e54858264238466c703
Aditya Subramanian <aditarun2008@gmail.com>
2024-04-19 21:39:02 -0400

    h t m X gon give it to ya

---
+++ server/app.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"adisuper94/turboguac/server/generated"
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"adisuper94/turboguac/wsmessagespec"
+
+	"nhooyr.io/websocket"
+)
+
+func getChatRooms(ctx context.Context, username string) ([]generated.ChatRoom, error) {
+	queries := GetQueries()
+	chatRooms, err := queries.GetChatRoomDetailsByUsername(ctx, username)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while getting chatrooms\n%v\n", err)
+		return nil, err
+	}
+	return chatRooms, nil
+}
+
+var hbs *template.Template
+
+func main() {
+	hbs = template.Must(template.ParseGlob("*.html"))
+	conn := getDBConn()
+	defer conn.Close()
+	server := Server{
+		clients: make(map[string]ActiveClient),
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		c, err := websocket.Accept(w, r, &websocket.AcceptOptions{InsecureSkipVerify: true})
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error1: ", err)
+			return
+		}
+		defer c.CloseNow()
+		addr := r.RemoteAddr
+		log.Printf("Client connected: %s", addr)
+		ctx, cancel := context.WithTimeout(r.Context(), 360*time.Second)
+		defer cancel()
+		defer delete(server.clients, addr)
+		for {
+			_, rawMessageBytes, err := c.Read(ctx)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error2: ", err)
+				break
+			}
+			var msg wsmessagespec.WSMessage
+			err = json.Unmarshal(rawMessageBytes, &msg)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error3: ", err)
+				break
+			}
+			log.Printf("Received: %s", msg.Data)
+			client, err := server.HandleMessage(msg, ctx, addr, c)
+			if client != nil {
+				defer delete(server.clients, client.userName)
+			}
+			if err != nil {
+				break
+			}
+		}
+
+		c.Close(websocket.StatusNormalClosure, "")
+	})
+
+	type Obj struct {
+		OnlineUsers []string
+	}
+	http.HandleFunc("/online-users", func(w http.ResponseWriter, r *http.Request) {
+		var activeUsers []string
+		for k := range server.clients {
+			activeUsers = append(activeUsers, k)
+		}
+		if getAcceptHeader(r) == HTML {
+			hbs.ExecuteTemplate(os.Stdin, "online-users", Obj{OnlineUsers: activeUsers})
+		} else {
+			json.NewEncoder(w).Encode(activeUsers)
+		}
+	})
+
+	http.HandleFunc("/chatrooms", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			chatRooms, err := getChatRooms(r.Context(), r.FormValue("username"))
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			json.NewEncoder(w).Encode(chatRooms)
+		case http.MethodPost:
+			chatRoomName := r.FormValue("chatroom_name")
+			username := r.FormValue("username")
+			chatRoom, err := createChatRoom(r.Context(), username, chatRoomName)
+			if err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				return
+			}
+			json.NewEncoder(w).Encode(chatRoom)
+		}
+	})
+
+	http.HandleFunc("/dms", func(w http.ResponseWriter, r *http.Request) {
+		queries := GetQueries()
+		username := r.FormValue("username")
+		dms, err := queries.GetDMs(r.Context(), username)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error while getting dms\n%v\n", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		json.NewEncoder(w).Encode(dms)
+	})
+
+	httpServer := &http.Server{
+		Addr:    ":8080",
+		Handler: nil,
+	}
+	log.Fatal(httpServer.ListenAndServe())
+}
+
+type httpAcceptType int
+
+const (
+	HTML httpAcceptType = iota
+	JSON
+	UNKNOWN
+)
+
+func getAcceptHeader(r *http.Request) httpAcceptType {
+	rawAcceptHeader := r.Header.Get("Accept")
+	acceptMimeTypes := strings.Split(rawAcceptHeader, ",")
+	accpetsJSON := false
+	accpetsHTML := false
+	for _, acceptMimeType := range acceptMimeTypes {
+		mimeTypeParts := strings.Split(acceptMimeType, ";")
+		mimeType := mimeTypeParts[0]
+		switch mimeType {
+		case "text/html":
+			accpetsHTML = true
+
+		case "application/json":
+			accpetsJSON = true
+		}
+	}
+
+	if accpetsJSON && accpetsHTML {
+		return UNKNOWN
+	}
+	if accpetsJSON {
+		return JSON
+	}
+	if accpetsHTML {
+		return HTML
+	}
+
+	return UNKNOWN
+}
+
+func askQuestion(question string) bool {
+	fmt.Printf("%s (y/N)?", question)
+	var isHttps bool
+	stdinReader := bufio.NewReader(os.Stdin)
+Loop:
+	for {
+		byte, _ := stdinReader.ReadByte()
+		switch byte {
+		case 'y', 'Y':
+			isHttps = true
+			break Loop
+		case 'n', 'N':
+			isHttps = false
+			break Loop
+		case '\n', '\r', ' ':
+			continue
+		default:
+			fmt.Println("Please enter y or n")
+		}
+	}
+	return isHttps
+}
