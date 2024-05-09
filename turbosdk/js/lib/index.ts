45605badaff92708b68225a1b68742e62874f806
Aditya Subramanian <aditarun2008@gmail.com>
2024-05-08 20:36:56 -0400

    js/ts turboguac client

---
+++ turbosdk/js/lib/index.ts
@@ -0,0 +1,54 @@
+"use strict";
+
+export enum WSMessageType {
+  Text = 0,
+  Login,
+  LoginAck,
+  Logout,
+  SingleTick,
+  AddMemberToChatRoom
+}
+
+export type WSMessage = {
+  id: string;
+  data: string;
+  to: string;
+  from: string;
+  type: WSMessageType;
+}
+
+export class TurboGuacClient {
+  private ws: WebSocket
+  private userName: string
+  private serverAddr: string
+
+  constructor(serverAddr: string, userName: string) {
+    this.serverAddr = serverAddr;
+    this.userName = userName;
+    this.ws = new WebSocket("ws://" + serverAddr + "/ws");
+  }
+  async createChatRoom(roomName: string) {
+    const url = `http://${this.serverAddr}/chatrooms?username=${this.userName}&chatroom_name=${roomName}`;
+    let response = await fetch(url, { method: "POST", headers: { "Accept": "application/json" } });
+    return response;
+  }
+
+  async getMyChatRooms() {
+    const url = `http://${this.serverAddr}/chatrooms?username=${this.userName}`;
+    let response = await fetch(url, { method: "GET", headers: { "Accept": "application/json" } });
+    return response;
+
+  }
+
+  async getOnlineUsers() {
+    const url = `http://${this.serverAddr}/online-users`;
+    let response = await fetch(url, { method: "GET", headers: { "Accept": "application/json" } });
+    return response;
+  }
+
+  async getDMs() {
+    const url = `http://${this.serverAddr}/dms?username=${this.userName}`;
+    let response = await fetch(url, { method: "GET", headers: { "Accept": "application/json" } });
+    return response;
+  }
+}
